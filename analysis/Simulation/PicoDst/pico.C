91512c63aeb85ea56eb9ec9926a7c2e339e831d6
Barak Schmookler <bschmookler1@gmail.com>
2026-06-04 12:41:12 -0500

    Updated PicoDst analysis

--- analysis/Simulation/PicoDst/pico.C
+++ analysis/Simulation/PicoDst/pico.C
@@ -1,4 +1,4 @@
-void pico(int do=2, const Char_t *inFile = "filelist_pythia.list") {
+void pico(int do=2, const Char_t *inFile = "infiles.lis") {
   gSystem->Load("libPhysics");
   gSystem->Load("St_base");
   gSystem->Load("StChain");
@@ -17,6 +17,7 @@ void pico(int do=2, const Char_t *inFile = "filelist_pythia.list") {
   
   if(do==0) gROOT->ProcessLine(Form(".x match.C+(\"%s\")",inFile));
   if(do==1) gROOT->ProcessLine(Form(".x dilepton.C+(\"%s\")",inFile));
-  if(do==2) gROOT->ProcessLine(Form(".x track_qa.C+(\"%s\")",inFile));
+  if(do==2) gROOT->ProcessLine(Form(".x mip_ana.C+(\"%s\")",inFile));
+  if(do==3) gROOT->ProcessLine(Form(".x fwd_qa.C+(\"%s\")",inFile));
 }
 
